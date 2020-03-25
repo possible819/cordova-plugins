6896075b2327783a933edaafd65aadf5c86bd3ee
jaylee.possible <jaylee.possible@gmail.com>
2020-03-25 14:57:53 +0900

    Plugins updated

--- CustomAlert/src/android/CustomAlert.java
+++ CustomAlert/src/android/CustomAlert.java
@@ -1,4 +1,4 @@
-package custom-plugin-alert;
+package custom.plugin.alert;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -41,7 +41,7 @@ public class CustomAlert extends CordovaPlugin {
 
     private void alert(String alertType, String title, String message, JSONArray buttons,
             final CallbackContext callbackContext) throws JSONException {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this.cordova.getActivity().getApplicationContext());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.cordova.getActivity());
         if (!title.isEmpty())
             builder.setTitle(title);
         builder.setMessage(message);
@@ -53,8 +53,8 @@ public class CustomAlert extends CordovaPlugin {
 
             switch (type) {
                 case BUTTON_TYPE_DEFAULT:
-                    builder.setNeutralButton(name, new AlertDialog.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int _) {
+                    builder.setPositiveButton(name, new AlertDialog.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int idx) {
                             dialog.dismiss();
                             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, name));
 
@@ -64,7 +64,7 @@ public class CustomAlert extends CordovaPlugin {
 
                 case BUTTON_TYPE_CANCEL:
                     builder.setNegativeButton(name, new AlertDialog.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int _) {
+                        public void onClick(DialogInterface dialog, int idx) {
                             dialog.dismiss();
                             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, name));
                         }
@@ -73,7 +73,7 @@ public class CustomAlert extends CordovaPlugin {
 
                 case BUTTON_TYPE_DESTRUCTIVE:
                     builder.setNegativeButton(name, new AlertDialog.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int _) {
+                        public void onClick(DialogInterface dialog, int idx) {
                             dialog.dismiss();
                             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, name));
                         }
@@ -85,5 +85,7 @@ public class CustomAlert extends CordovaPlugin {
                     callbackContext.error("Button type " + type + " is available.");
             }
         }
+
+        builder.show();
     }
 }
---
+++ CustomAppStatusDispatcher/src/android/CustomAppStatusDispatcher.java
@@ -0,0 +1,21 @@
+package com.example.jaylee.myapplication;
+
+import org.apache.cordova.CordovaPlugin;
+import android.app.Activity;
+
+public class CustomAppStatusDispatcher extends CordovaPlugin {
+    @Override
+    public void onResume(boolean multitasking) {
+        this.webView.sendJavascript();
+    }
+
+    @Override
+    public void onPause(boolean multitasking) {
+
+    }
+
+    @Override
+    public void onStop() {
+
+    }
+}
