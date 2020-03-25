b3c492a16ad00e9b4f8310db9d422a97691bd305
Jay Lee <jaylee.possible@gmail.com>
2020-03-25 16:01:48 +0900

    Fixed wrong param for android platform (CustomAlert)

--- CustomAlert/src/android/CustomAlert.java
+++ CustomAlert/src/android/CustomAlert.java
@@ -13,8 +13,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 
 public class CustomAlert extends CordovaPlugin {
-
-    private static final String ACTION_ALERT = "alert";
     private static final String ACTION_DISMISS_ALERT = "dismissAlert";
 
     private static final String BUTTON_TYPE_DEFAULT = "DEFAULT";
@@ -23,24 +21,23 @@ public class CustomAlert extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        String alertType = args.getString(0);
         String title = args.getString(1);
         String message = args.getString(2);
         JSONArray buttons = args.getJSONArray(3);
         boolean result;
 
-        if (action.equals(ACTION_ALERT)) {
-            this.alert(alertType, title, message, buttons, callbackContext);
+        try {
+            this.alert(title, message, buttons, callbackContext);
             result = true;
-        } else {
+        } catch (Exception e) {
             result = false;
         }
 
         return result;
     }
 
-    private void alert(String alertType, String title, String message, JSONArray buttons,
-            final CallbackContext callbackContext) throws JSONException {
+    private void alert(String title, String message, JSONArray buttons, final CallbackContext callbackContext)
+            throws JSONException {
         AlertDialog.Builder builder = new AlertDialog.Builder(this.cordova.getActivity());
         if (!title.isEmpty())
             builder.setTitle(title);
@@ -63,7 +60,7 @@ public class CustomAlert extends CordovaPlugin {
                     break;
 
                 case BUTTON_TYPE_CANCEL:
-                    builder.setNegativeButton(name, new AlertDialog.OnClickListener() {
+                    builder.setNeutralButton(name, new AlertDialog.OnClickListener() {
                         public void onClick(DialogInterface dialog, int idx) {
                             dialog.dismiss();
                             callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, name));
