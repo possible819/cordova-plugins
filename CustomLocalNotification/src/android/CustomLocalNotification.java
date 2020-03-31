db8ebd25df97dad4c73d554e8904f0a3a40a1c30
Jay Lee <jaylee.possible@gmail.com>
2020-03-31 11:26:38 +0900

    update local notification for android

--- CustomLocalNotification/src/android/CustomLocalNotification.java
+++ CustomLocalNotification/src/android/CustomLocalNotification.java
@@ -4,13 +4,10 @@ import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.job.JobParameters;
-import android.app.job.JobService;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.os.SystemClock;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -21,17 +18,15 @@ import java.util.Calendar;
 
 import androidx.core.app.NotificationCompat;
 
-/**
- * This class echoes a string called from JavaScript.
- */
 public class CustomLocalNotification extends CordovaPlugin {
 
     private final String ACTION_TYPE_ADD = "addNotification";
     private final String ACTION_TYPE_CLEAR = "clearNotification";
-    private final int REQUEST_CODE = 19870819;
-    private final int NOTIFICATION_ID = 19870819;
+    private final int REQUEST_CODE = 0;
+    private final int NOTIFICATION_ID = 1491894352;
     private final String NOTIFICATION_CHANNEL_ID = "CST_LOCAL_NOTIFICATION_CHN_ID";
     private final String CHANNEL_NAME = "CST_LOCAL_NOTIFICATION_NAME";
+    private final int INTENT_FLAG = 0;
 
     private Context context;
     private String contentTitle;
@@ -44,9 +39,10 @@ public class CustomLocalNotification extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         this.context = this.cordova.getContext();
+        this.contentTitle = args.getString(0);
         boolean result = false;
+
         if (action.equals(ACTION_TYPE_ADD)) {
-            this.contentTitle = args.getString(0);
             this.subText = args.getString(1);
             this.contentText = args.getString(2);
             this.hour = args.getInt(3);
@@ -76,29 +72,24 @@ public class CustomLocalNotification extends CordovaPlugin {
     private void registerNotification() {
         Intent intent = new Intent(this.context, NotificationReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this.context, this.REQUEST_CODE, intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+                this.INTENT_FLAG);
 
         AlarmManager alarmManager = (AlarmManager) this.context.getSystemService(Context.ALARM_SERVICE);
 
-        // Calendar calendar = Calendar.getInstance();
-        // calendar.set(Calendar.HOUR_OF_DAY, this.hour);
-        // calendar.set(Calendar.MINUTE, this.minute);
-        // calendar.set(Calendar.SECOND, 0);
-
-        // alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
-        // calendar.getTimeInMillis(), 10 * 1000, pendingIntent);
-
         Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.add(Calendar.SECOND, 10);
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 10 * 1000, pendingIntent);
+        calendar.set(Calendar.HOUR_OF_DAY, this.hour);
+        calendar.set(Calendar.MINUTE, this.minute);
+        calendar.set(Calendar.SECOND, 0);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY,
+                pendingIntent);
     }
 
     private void clearNotification() {
         try {
             Intent intent = new Intent(this.context, NotificationReceiver.class);
             PendingIntent pendingIntent = PendingIntent.getService(this.context, this.REQUEST_CODE, intent,
-                    PendingIntent.FLAG_UPDATE_CURRENT);
+                    this.INTENT_FLAG);
             AlarmManager alarmManager = (AlarmManager) this.context.getSystemService(Context.ALARM_SERVICE);
             alarmManager.cancel(pendingIntent);
             this.callbackContext.success();
@@ -115,7 +106,7 @@ public class CustomLocalNotification extends CordovaPlugin {
                     .getSystemService(context.NOTIFICATION_SERVICE);
 
             NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-                    .setSmallIcon(android.R.drawable.alert_light_frame).setContentTitle(contentTitle)
+                    .setSmallIcon(android.R.drawable.ic_menu_edit).setContentTitle(contentTitle)
                     .setContentText(contentText).setSubText(subText).setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -125,42 +116,6 @@ public class CustomLocalNotification extends CordovaPlugin {
             }
 
             notificationManager.notify(NOTIFICATION_ID, builder.build());
-
-        }
-    }
-
-    class NotificationService extends JobService {
-        @Override
-        public boolean onStartJob(JobParameters params) {
-            boolean result = true;
-            try {
-                NotificationManager notificationManager = (NotificationManager) context
-                        .getSystemService(context.NOTIFICATION_SERVICE);
-
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-                        .setSmallIcon(android.R.drawable.alert_light_frame).setContentTitle(contentTitle)
-                        .setContentText(contentText).setSubText(subText)
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    final CharSequence CHANNEL_NAME = "CST_LOCAL_NOTIFICATION_NAME";
-                    NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, CHANNEL_NAME,
-                            NotificationManager.IMPORTANCE_DEFAULT);
-                    notificationManager.createNotificationChannel(channel);
-                }
-
-                notificationManager.notify(NOTIFICATION_ID, builder.build());
-
-            } catch (Exception e) {
-                result = false;
-            }
-
-            return result;
-        }
-
-        @Override
-        public boolean onStopJob(JobParameters params) {
-            return false;
         }
     }
 }
